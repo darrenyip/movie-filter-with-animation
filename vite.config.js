3d8f01c899aa88469cd2030d9c993acb17da5ccc
zhangyu ye <darrenyipzy@gmail.com>
2022-05-07 20:11:21 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    PublicPath: "./",
+    // PublicPath: "./",
+    base: "/movie-filter-with-animation/",
 });
